a6e54e8f78c88b52b2940a3f5ded432c72e9e784
george-simon <george-simon@users.noreply.github.com>
2020-10-17 16:41:45 -0700

    circles and labels

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -19,9 +19,16 @@ var svg = d3.select("#scatter")
 //Read the data
 d3.csv("./assets/data/data.csv").then(riskData => {
     console.log(riskData);
+
+    // parse data
+    riskData.forEach(data => {
+      data.poverty = +data.poverty;
+      data.healthcare = +data.healthcare;
+    });
+
     // Add X axis
     var x = d3.scaleLinear()
-      .domain([0, 4000])
+      .domain([0, d3.max(riskData, d => d.poverty)])
       .range([ 0, width ]);
     svg.append("g")
       .attr("transform", "translate(0," + height + ")")
@@ -29,7 +36,7 @@ d3.csv("./assets/data/data.csv").then(riskData => {
 
     // Add Y axis
     var y = d3.scaleLinear()
-      .domain([0, 500000])
+      .domain([0, d3.max(riskData, d => d.healthcare)])
       .range([ height, 0]);
     svg.append("g")
       .call(d3.axisLeft(y));
@@ -37,12 +44,35 @@ d3.csv("./assets/data/data.csv").then(riskData => {
     // Add dots
     svg.append('g')
       .selectAll("dot")
-      .data(data)
+      .data(riskData)
       .enter()
       .append("circle")
         .attr("cx", function (d) { return x(d.poverty); } )
-        .attr("cy", function (d) { return y(d.healthcareLow); } )
-        .attr("r", 1.5)
+        .attr("cy", function (d) { return y(d.healthcare); } )
+        .attr("r", 7)
+        .attr("opacity", 0.5)
+        .attr("stroke", "black")
         .style("fill", "#69b3a2")
+
+
+    // Create group for x-axis label
+    var labelsGroup = svg.append("g")
+      .attr("transform", `translate(${width / 2}, ${height + 20})`);
+
+    var povertyLabel = labelsGroup.append("text")
+      .attr("x", 0)
+      .attr("y", 20)
+      .classed("active", true)
+      .text("Poverty (%)");
+
+    // append y axis
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .classed("axis-text", true)
+      .text("Number of Billboard 500 Hits");
+
 
 }).catch(error => console.log(error));
