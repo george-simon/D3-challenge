28ad22b41f1968dcea2c86266cc05698256070bc
george-simon <george-simon@users.noreply.github.com>
2020-10-17 17:00:41 -0700

    graph scaling

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -2,16 +2,18 @@
 // Reference: https://www.d3-graph-gallery.com/graph/scatter_basic.html
 //= ================================
 // set the dimensions and margins of the graph
-var margin = {top: 20, right: 40, bottom: 50, left: 70},
-    width = 460 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+var svgWidth = 960;
+var svgHeight = 500;
 
+var margin = {top: 20, right: 40, bottom: 50, left: 70},
+    width = svgWidth - margin.left - margin.right,
+    height = svgHeight - margin.top - margin.bottom;
 
 // append the svg object to the body of the page
 var svg = d3.select("#scatter")
   .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+    .attr("width", svgWidth) // width + margin.left + margin.right
+    .attr("height", svgHeight) //height + margin.top + margin.bottom
   .append("g")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
@@ -49,7 +51,7 @@ d3.csv("./assets/data/data.csv").then(riskData => {
       .append("circle")
         .attr("cx", function (d) { return x(d.poverty); } )
         .attr("cy", function (d) { return y(d.healthcare); } )
-        .attr("r", 7)
+        .attr("r", 11)
         .attr("opacity", 0.5)
         .attr("stroke", "black")
         .style("fill", "#69b3a2")
